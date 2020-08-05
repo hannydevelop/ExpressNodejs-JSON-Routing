732eeb037ce3b2d59869a5d3a264ea83bf32ae87
hannydevelop <ukpaiugochiibem@gmail.com>
2020-08-05 14:55:24 +0100

    pushing to github

--- API/mockApi.js
+++ API/mockApi.js
@@ -7,24 +7,30 @@ router.get('/choice', (req, res) => {
     res.json(db.choices)
 });
 
-router.get('/choice', (req, res) => {
+router.get('/choice/1', (req, res) => {
     //get an array of first choice only
-    res.json(db.choices)
+    res.json(db.choices[0])
 });
 
-router.get('/choice', (req, res) => {
+router.get('/choice/2', (req, res) => {
     //get an array of second choice only
-    res.json(db.choices)
+    res.json(db.choices[1])
 });
 
-router.get('/choice', (req, res) => {
+router.get('/choice/inner', (req, res) => {
     //get value from first choice parameter
-    res.json(db.choices)
+    res.json(db.choices[1].title)
     //this can be used getting first or second choice parameter
+});
+
+router.get('/outer', (req, res) => {
+    //get value not in an array
+    res.json(db.completeness)
 });
 
 router.post('/choice', (req, res) => {
     const choice = req.body
+    //this can be tested with postman
     res.json(choice)
 });
 
